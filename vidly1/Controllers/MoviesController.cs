7fcd54d0d747bd2de406892445b43206c900adef
richardsoriano <soriano.richard2020@gmail.com>
2022-12-06 22:29:07 -0800

    F

--- vidly1/Controllers/MoviesController.cs
+++ vidly1/Controllers/MoviesController.cs
@@ -13,15 +13,12 @@ namespace vidly1.Controllers
         {
             _context = context;
         }
-<<<<<<< HEAD
          public ViewResult Index()
         {
             var movies = _context.Movies.Include(m=>m.GenreType).ToList();
             return View(movies);
         }
 
-=======
->>>>>>> c16370ff062cf943f08d470515b56fade20f828e
         public IActionResult Random()
         {
            var movie = new Movie() { Name = "Shrek!" };
@@ -40,33 +37,11 @@ namespace vidly1.Controllers
 
         public ActionResult Details(int id)
         {
-<<<<<<< HEAD
             var movie = _context.Movies.Include(m => m.GenreType).SingleOrDefault(m => m.Id == id);
             if (movie == null)
                 return Content("Not Found");
             return View(movie);
 
-=======
-            return Content("id=" + movieId);
-        }
-        public IActionResult Index(int? pageIndex, string sortBy) 
-        {
-            //if (!pageIndex.HasValue)
-            //    pageIndex = 1;
-            //if (String.IsNullOrWhiteSpace(sortBy))
-            //    sortBy = "Name";
-            //return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex, sortBy));
-            var movies = _context.Movies.Include(m => m.GenreType).ToList();
-            //var customers = _context.Customers.Include(c => c.Name).ToList();
-            return View(movies);
-        }
-        [Route("movies/released/{year}/{month}")]
-        public IActionResult ByReleaseDate(int year, int month)
-        {
-             return Content(year + "/" + month);
-            //return Content("Hello");
-            
->>>>>>> c16370ff062cf943f08d470515b56fade20f828e
         }
         protected override void Dispose(bool disposing) { _context.Dispose(); }
 
